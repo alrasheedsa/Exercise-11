0054d0f64126e4fb181dfe262dfc5783d85a57df
Mohammed Alrasheed <m.alrasheedsaa@gmail.com>
2026-04-21 14:44:24 +0300

    Add files via upload

---
+++ excercise11/Excercise11Application.java
@@ -0,0 +1,13 @@
+package org.example.excercise11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Excercise11Application.class, args);
+    }
+
+}
